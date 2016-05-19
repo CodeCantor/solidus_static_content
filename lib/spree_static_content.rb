774e09d7714194242400a59340ca3c7553edd52d
Sergio Arbeo <serabe@gmail.com>
2016-05-19 19:57:11 +0200

    Fix constrain

--- lib/spree_static_content.rb
+++ lib/spree_static_content.rb
@@ -13,6 +13,7 @@ end
 class Spree::StaticPage
   def self.matches?(request)
     return false if request.path =~ /(^\/+(admin|account|cart|checkout|content|login|pg\/|orders|products|s\/|session|signup|shipments|states|t\/|tax_categories|user)+)/
-    !Spree::Page.visible.find_by_slug(request.path).nil?
+    slug = StaticPage.remove_spree_mount_point request.path
+    Spree::Page.visible.where(slug: slug).exists?
   end
 end
